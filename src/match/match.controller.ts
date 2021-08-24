e0b8f82b5721e33db31e4e09853a411521199b2c
Bianca Lujan <bblanc545@aol.com>
2021-08-24 09:32:56 -0600

    outlines for match service/controller done

--- src/match/match.controller.ts
+++ src/match/match.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateMatchDto } from './dtos/create-match.dto';
 
 @Controller('match')
-export class MatchController {}
+export class MatchController {
+  /*
+    POST /match
+    Create a match
+  */
+  @Post()
+  createMatch(@Body() createMatchDto: CreateMatchDto) {
+    return 'creates a new match';
+  }
+
+  /*
+    GET /match/{id}
+    Find a match by id
+  */
+  @Get(':id')
+  findMatch(@Param('id') id: string) {
+    return 'finds a match';
+  }
+
+  /*
+    PATCH /{id}
+    Updates a match
+  */
+  @Patch(':id')
+  updateMatch(@Param('id') id: string, @Body() body: string) {
+    return 'updates a match';
+  }
+
+  /*
+    Delete /match/{id}
+    Deletes a match
+  */
+  @Delete(':id')
+  deleteMatch(@Param('id') id: string) {
+    return 'deletes a match';
+  }
+}
--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { Match } from './models/match.entity';
 
 @Injectable()
-export class MatchService {}
+export class MatchService {
+  private readonly matches: Match[] = [];
+
+  /*
+    POST /match
+    Create a match
+  */
+  createMatch(createMatchDto: CreateMatchDto) {
+    return 'creates a new match';
+  }
+
+  /*
+      GET /match/{id}
+      Find a match by id
+    */
+  findMatch(id: string) {
+    return 'finds a match';
+  }
+
+  /*
+      PATCH /{id}
+      Updates a match
+    */
+  updateMatch(id: string, body: string) {
+    return 'updates a match';
+  }
+
+  /*
+      Delete /match/{id}
+      Deletes a match
+    */
+  deleteMatch(id: string) {
+    return 'deletes a match';
+  }
+}

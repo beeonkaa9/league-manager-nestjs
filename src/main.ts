e0df0d5e403026688215bbba890b19f6f0a65abd
Bianca Lujan <bblanc545@aol.com>
2021-09-16 10:52:27 -0600

    swagger installation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('League Manager')
+    .setDescription('NestJS manager for a soccer league')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
